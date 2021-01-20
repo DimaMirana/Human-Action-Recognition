3e3488ab6d726bd6169a4aad5c7e678032469a91
Tasnim Ferdous Dima <tasnimdima25@gmail.com>
2021-01-20 15:08:33 +0600

    human activity recognition

--- human_activity_recognition.py
+++ human_activity_recognition.py
@@ -2,6 +2,7 @@
 # python human_activity_recognition.py 
 
 import numpy as np
+import imutils
 import sys
 import cv2
 
@@ -15,54 +16,54 @@ net = cv2.dnn.readNet('resnet-34_kinetics.onnx')
 
 # grab a pointer to the input video stream
 print("[INFO] accessing video stream...")
-vs = cv2.VideoCapture('example_activities.mp4')
+vs = cv2.VideoCapture('Shoplifting001_x264_0.mp4')
 
 # loop until we explicitly break from it
 while True:
-	# initialize the batch of frames that will be passed through the
-	# model
-	frames = []
+    # initialize the batch of frames that will be passed through the
+    # model
+    frames = []
 
-	# loop over the number of required sample frames
-	for i in range(0, SAMPLE_DURATION):
-		# read a frame from the video stream
-		(ret, frame) = vs.read()
+    # loop over the number of required sample frames
+    for i in range(0, SAMPLE_DURATION):
+        # read a frame from the video stream
+        (ret, frame) = vs.read()
 
-		# if the frame was not ret then we've reached the end of
-		# the video stream so exit the script
-		if not ret:
-			print("[INFO] no frame read from stream - exiting")
-			sys.exit(0)
+        # if the frame was not ret then we've reached the end of
+        # the video stream so exit the script
+        if not ret:
+            print("[INFO] no frame read from stream - exiting")
+            sys.exit(0)
 
-		# otherwise, the frame was read so resize it and add it to
-		# our frames list
-		frame = imutils.resize(frame, width=400)
-		frames.append(frame)
+        # otherwise, the frame was read so resize it and add it to
+        # our frames list
+        frame = imutils.resize(frame, width=400)
+        frames.append(frame)
 
-	#construct the blob
-	blob = cv2.dnn.blobFromImages(frames, 1.0,
+    #construct the blob
+    blob = cv2.dnn.blobFromImages(frames, 1.0,
                                 (SAMPLE_SIZE, SAMPLE_SIZE), 
                                 (114.7748,107.7354, 99.4750),
                                 swapRB=True, crop=True)
-	blob = np.transpose(blob, (1, 0, 2, 3))
-	blob = np.expand_dims(blob, axis=0)
+    blob = np.transpose(blob, (1, 0, 2, 3))
+    blob = np.expand_dims(blob, axis=0)
 
-	# pass the blob through the network to obtain our human activity
-	# recognition predictions
-	net.setInput(blob)
-	outputs = net.forward()
-	label = CLASSES[np.argmax(outputs)]
+    # pass the blob through the network to obtain our human activity
+    # recognition predictions
+    net.setInput(blob)
+    outputs = net.forward()
+    label = CLASSES[np.argmax(outputs)]
 
-	# loop over our frames
-	for frame in frames:
-		# draw the predicted activity on the frame
-		cv2.rectangle(frame, (0, 0), (300, 40), (0, 0, 0), -1)
-		cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
+    # loop over our frames
+    for frame in frames:
+        # draw the predicted activity on the frame
+        cv2.rectangle(frame, (0, 0), (300, 40), (0, 0, 0), -1)
+        cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
                     0.8, (255, 255, 255), 2)
 
-		# display the frame to our screen
-		cv2.imshow("Activity Recognition", frame)
-		key = cv2.waitKey(1) & 0xFF
+        # display the frame to our screen
+        cv2.imshow("Activity Recognition", frame)
+        key = cv2.waitKey(1) & 0xFF
         
-		if key == ord("q"):
-    			break
+        if (cv2.waitKey(10) & 0xFF == ord('q')):
+                break
